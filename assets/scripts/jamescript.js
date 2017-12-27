663987389f061d645af6c0588712875dcda727c4
hedgehog125 <username@users.noreply.github.com>
2017-12-27 18:09:05 +0000

    Added getCookie, setCookie and deleteCookie.

--- assets/scripts/jamescript.js
+++ assets/scripts/jamescript.js
@@ -59,6 +59,38 @@ function post(url, data, code) {
 	http.send(params)
 }
 
+// W3schools cookie thing. (https://www.w3schools.com/js/js_cookies.asp)
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function deleteCookie(cname) {
+	var d = new Date();
+    d.setTime(d.getTime() - (100*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + "" + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 function get(url, func) {
 	// From https://stackoverflow.com/questions/9713058/send-post-data-using-xmlhttprequest and https://stackoverflow.com/questions/18962799/javascript-http-post-with-json-data
     http = new XMLHttpRequest()
